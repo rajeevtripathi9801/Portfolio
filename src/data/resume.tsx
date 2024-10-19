b5d3c0ad56d4701f671d632f4bd0996de7b7ca72
tripathirajeev1009 <tripathirajeev1009@gmail.com>
2024-10-19 21:46:08 +0530

    spell check

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -10,7 +10,7 @@ export const DATA = {
   description:
     "Software Engineer building 5G systems for India.",
   summary:
-    "Software Engineer currently working at Nokia, India building and optimising 5G systems of India. I pursued a bachelors degree in computer science where I interned at Nokia for around 10 months in my final year, prior to that cofounded a ecommerce and reached net revenue of $10k in 10 months of opertation. Academically I also got my paper accepted in ICBAR on topic of Distributed System using NodeJS and MongoDB.",
+    "Software Engineer currently working at Nokia, India building and optimising 5G systems of India. I pursued a bachelors degree in computer science where I interned at Nokia for around 10 months in my final year, prior to that cofounded a ecommerce and reached net revenue of $10k in 10 months of operation. Academically I also got my paper accepted in ICBAR on topic of Distributed System using NodeJS and MongoDB.",
   avatarUrl: "/me.jpg",
   skills: [
     "C++",
